f7bc2dd1e7408189862af976ffc8ed9c15e8dda2
MiguelRibeiro55 <66682524+MiguelRibeiro55@users.noreply.github.com>
2020-06-09 14:47:22 +0100

    Update aluguer exemplo.sh

--- aluguer exemplo.sh
+++ aluguer exemplo.sh
@@ -1,1 +1,2 @@
-insert into aluger(id_utilizador, id_bot) select id from data_users  and select id from Bots 
+insert into aluguer (id_utilizador, id_bot) select id from data_users  and select id from Bots 
+insert into aluguer (datainicio, ip, datafim) values ("" ,"", "")
